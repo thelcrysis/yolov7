5a136417e696a2137518f733a9a8fdbaf84f9f83
Ljudevit Jelečević <thelcrysis@gmail.com>
2023-04-26 19:50:16 +0200

    fixing device differ

--- utils/loss.py
+++ utils/loss.py
@@ -1400,6 +1400,7 @@ class ComputeLossAuxOTA:
                 matching_matrix[:, anchor_matching_gt > 1] *= 0.0
                 matching_matrix[cost_argmin, anchor_matching_gt > 1] = 1.0
             fg_mask_inboxes = matching_matrix.sum(0) > 0.0
+            fg_mask_inboxes = fg_mask_inboxes.to(torch.device('cuda'))
             matched_gt_inds = matching_matrix[:, fg_mask_inboxes].argmax(0)
         
             from_which_layer = from_which_layer[fg_mask_inboxes]
